c2823bf81a8f59e884c6254dcc75bb95ab44459d
Mohammad Barghamadi <mohammadbarghamadi@gmail.com>
2024-05-01 12:38:00 +0330

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-x3l font-bold underline">این برنامه آموزش تیلویند می‌باشد</h1>
+            <p>در این بخش میتوانم درباره تیلویند سی اس اس بنویسم و اینکه چگونه میوان با آن اطلاعات خوبی حمل کرد.</p>
+        </div>
+    )
+}
+
+export default App
